bdfcda7bc6a03db58c463c2ce17c92074a0cc77d
Irgendj0 <42973022+Irgendj0@users.noreply.github.com>
2024-06-02 09:27:48 +0200

    add updateLocation function

--- UnityProjekte/Base Project/Assets/Scripts/WeatherGetter.cs
+++ UnityProjekte/Base Project/Assets/Scripts/WeatherGetter.cs
@@ -9,14 +9,13 @@ public class WeatherGetter : MonoBehaviour
     //API Key is hidden inside a static class
     private string apiKey = APIKey.apiKey;
     private string baseURL = "https://api.openweathermap.org/data/2.5/weather?appid=";
-   [SerializeField] private float lat = 42.2f;
-   [SerializeField] private float lon = 12.0f;
+   //will be null unless something has been requested
     private string result = null;
     
     // Start is called before the first frame update
     void Start()
     {
-        Coroutine rout =   StartCoroutine(GetRequest(lat,lon));
+        
         
     }
 
@@ -24,22 +23,19 @@ public class WeatherGetter : MonoBehaviour
     void Update()
     {
 
-        if(result != null) {
-        Root res = JsonUtility.FromJson<Root>(result);
-        }
     }
     IEnumerator GetRequest(float lat, float lon)
 {
     //using (UnityWebRequest www = UnityWebRequest.Get(baseURL+"&appid=" + apiKey +"&lat=" + lat + "&lon=" + lon ))
     using (UnityWebRequest www = UnityWebRequest.Get(baseURL + apiKey + "&lat=" + lat + "&lon=" + lon ))
     {
-        Debug.Log(baseURL+"&lat=" + lat + "&lon=" + lon + "&appid=" + apiKey);
+        
         yield return www.SendWebRequest();
 
         if (www.result == UnityWebRequest.Result.Success)
         {
-            Debug.Log("Received: " + www.downloadHandler.text);
             result = www.downloadHandler.text;
+            Root res = JsonUtility.FromJson<Root>(result);
         }
         else
         {
@@ -48,17 +44,22 @@ public class WeatherGetter : MonoBehaviour
     } // The using block ensures www.Dispose() is called when this block is exited
 
 }
-public currentWeather getCurrentWeather(){
+// this will return the current weather if something was requested. Otherwise it will continue return null, unless something arrives
+public currentWeather getWeather(){
         if (result != null) {
             Root res = JsonUtility.FromJson<Root>(result);
-            // Weather wea = JsonUtility.FromJson<Weather>(result);
 
             Weather[] wea = res.weather.ToArray();
 
-            Debug.Log("Current Weather is updated");
             return new currentWeather(res.main.temp, wea[0].id, wea[0].main, wea[0].description, wea[0].icon);
     }
     else return null;
 
+}
+//this start a lookup for the weather at a new location, it invalidates the currently cached weather and makes no guarantee that something new will replace it (eg. the internet may be down)
+public void updateLocation(float lat, float lon){
+    result = null;
+    //start coroutine
+    Coroutine rout = StartCoroutine(GetRequest(lat,lon));
 }
 }
--- UnityProjekte/Base Project/Assets/Scripts/weatherConsumer.cs
+++ UnityProjekte/Base Project/Assets/Scripts/weatherConsumer.cs
@@ -3,21 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class weatherConsumer : MonoBehaviour {
+    [SerializeField] private float lat = 42.2f;
+    [SerializeField] private float lon = 12.0f;
     public WeatherGetter weatherGetter;
+    currentWeather weather;
+    bool printed = false;
     // Start is called before the first frame update
     void Start() {
-
+        //set random example location
+        weatherGetter.updateLocation(lat,lon);
     }
 
     // Update is called once per frame
     void Update() {
-        
-        currentWeather weather = weatherGetter.getCurrentWeather();
+        currentWeather weather = weatherGetter.getWeather();
         if (weather == null) Debug.Log("Waiting for request");
         else {
+            if(!printed){
+                
+                Debug.Log("Temp:" + weather.temp);
+                Debug.Log(weather.description);
+                printed = true;
+            }
 
-            Debug.Log(weather.temp);
-            Debug.Log(weather.description);
 
         }
     }
