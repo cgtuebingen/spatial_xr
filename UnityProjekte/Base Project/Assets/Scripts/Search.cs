87bf8c3c0f5fb2458d9e2f66e226a7283fe5179a
Irgendj0 <42973022+Irgendj0@users.noreply.github.com>
2024-06-14 17:58:46 +0200

    implement search to location

---
+++ UnityProjekte/Base Project/Assets/Scripts/Search.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//wtf
+using UnityEngine.XR.Interaction.Toolkit.Samples.SpatialKeyboard;
+public class Search : MonoBehaviour
+{
+    public WeatherGetter weatherGetter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision collision){
+        GlobalNonNativeKeyboard.instance.ShowKeyboard();
+    }
+    public void submit(){
+        weatherGetter.updateLocationFromString(GlobalNonNativeKeyboard.instance.keyboard.text);
+        Debug.Log(GlobalNonNativeKeyboard.instance.keyboard.text);
+    }
+}
--- UnityProjekte/Base Project/Assets/Scripts/TouchPositionFinder.cs
+++ UnityProjekte/Base Project/Assets/Scripts/TouchPositionFinder.cs
@@ -2,10 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class TouchPositionFinder : MonoBehaviour
 {
     public float correction = 155.0f;
