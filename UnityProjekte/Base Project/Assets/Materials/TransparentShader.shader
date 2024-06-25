bad0bb466eec58d0fc9d9ffaf881d3f0a3f0e271
Irgendj0 <42973022+Irgendj0@users.noreply.github.com>
2024-06-25 07:16:05 +0200

    implement tiling shader

--- UnityProjekte/Base Project/Assets/Materials/TransparentShader.shader
+++ UnityProjekte/Base Project/Assets/Materials/TransparentShader.shader
@@ -3,7 +3,9 @@ Shader "Custom/NewSurfaceShader"
     Properties
     {
         _MainTex ("Texture", 2D) = "white" {}
-        _Color ("Main Color", Color) = (1,1,1,1)
+        //_Color ("Main Color", Color) = (1,1,1,1)
+        _TexList ("Map Parts", 2DArray) = "white" {}
+        _ZoomLevel ("Zoom Level", Integer) = 0
     }
     SubShader
     {
@@ -24,37 +26,57 @@ Shader "Custom/NewSurfaceShader"
             CGPROGRAM
             #pragma vertex vert
             #pragma fragment frag
-
+            #pragma require 2darray
             #include "UnityCG.cginc"
 
             struct appdata
             {
                 float4 vertex : POSITION;
+                float3 normal: NORMAL;
                 float2 uv : TEXCOORD0;
             };
 
             struct v2f
             {
                 float2 uv : TEXCOORD0;
+                float3 normal : NORMAL;
                 float4 vertex : SV_POSITION;
             };
 
-            sampler2D _MainTex;
+            //sampler2D _MainTex;
             float4 _MainTex_ST;
-            float4 _Color;
-
+            //float4 _Color;
+            int _ZoomLevel;
+            UNITY_DECLARE_TEX2DARRAY(_TexList);
             v2f vert (appdata v)
             {
                 v2f o;
                 o.vertex = UnityObjectToClipPos(v.vertex);
+                o.normal = normalize(v.normal);
+                //float longitude = atan2(normal.z, normal.x);
+                //float latitude = asin(normal.y);
+
+                //o.uv.x = (longitude / UNITY_PI) * 0.5 + 0.5;
+                //o.uv.y = (latitude / (UNITY_PI * 0.5)) * 0.5 + 0.5;
                 o.uv = TRANSFORM_TEX(v.uv, _MainTex);
                 return o;
             }
 
             fixed4 frag (v2f i) : SV_Target
             {
-                fixed4 texColor = tex2D(_MainTex, i.uv);
-                return texColor * _Color;
+                float longitude = atan2(i.normal.z, i.normal.x);
+                float latitude = asin(i.normal.y);
+
+                i.uv.x = (longitude / UNITY_PI) * 0.5 + 0.5;
+                i.uv.y = (latitude / (UNITY_PI * 0.5)) * 0.5 + 0.5;
+                //fixed4 texColor = tex2D(_MainTex, i.uv);
+                float tileSize = pow(2,_ZoomLevel);
+                int index = int(i.uv.x*tileSize) + int(tileSize-i.uv.y*tileSize)*tileSize;
+                //if(i.uv.y > 0.5) index = 3;
+                float2 uv = frac(i.uv * tileSize);
+                fixed4 texColor = UNITY_SAMPLE_TEX2DARRAY(_TexList,float3(uv,index));
+                //texColor = fixed4(uv,0,1);
+                return texColor;
             }
             ENDCG
         }
--- UnityProjekte/Base Project/Assets/Scripts/WeatherMapGetter.cs
+++ UnityProjekte/Base Project/Assets/Scripts/WeatherMapGetter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,16 +11,43 @@ public class WeatherMapGetter : MonoBehaviour
     private string apiKey = APIKey.apiKey;
     public string layer;
     private string baseURl = "https://tile.openweathermap.org/map/";
+    public int zoomLevel = 0;
+    private Texture2DArray textures;
     void Start()
     {
+        textures = new Texture2DArray(256, 256, (int)Math.Pow(4, zoomLevel), TextureFormat.ARGB32, true);
         material = GetComponent<Renderer>().material;
-        material.mainTexture = Texture2D.blackTexture;
-        StartCoroutine(GetText(layer, 0, 0, 0));
+        for (int x = 0; x < (int)Math.Pow(2, zoomLevel); x++)
+        {
+            for (int y = 0; y < (int)Math.Pow(2, zoomLevel); y++)
+            {
+                Debug.Log("" + x + y + zoomLevel);
+                StartCoroutine(GetText(layer, x, y, zoomLevel));
+            }
+        }
+        material.SetTexture("_TexList", textures);
+        material.SetInteger("_ZoomLevel",zoomLevel);
+    }
+
+    private void Update()
+    {
+        gameObject.transform.Rotate(Vector3.down,0.1f);
     }
 
     IEnumerator GetText(string layer, int x, int y, int z)
     {
-        string url = baseURl + layer + "/"+x+"/"+y+"/"+z+".png?appid=" + apiKey;
+        if (false)
+        {
+            Color[] colors = new Color[256*256];
+            Array.Fill(colors, Color.black);
+            textures.SetPixels(colors,1,0);
+            textures.Apply(true);
+
+            yield break;
+                
+        }
+        string url = baseURl + layer + "/"+z+"/"+x+"/"+y+".png?appid=" + apiKey;
+        Debug.Log(url);
         using (UnityWebRequest uwr = UnityWebRequestTexture.GetTexture(url))
         {
             yield return uwr.SendWebRequest();
@@ -31,8 +59,12 @@ public class WeatherMapGetter : MonoBehaviour
             else
             {
                 // Get downloaded asset bundle
-                Debug.Log(DownloadHandlerTexture.GetContent(uwr).format);
-                material.mainTexture = DownloadHandlerTexture.GetContent(uwr);
+                textures.SetPixels(DownloadHandlerTexture.GetContent(uwr).GetPixels(),y*(int)Math.Pow(2,zoomLevel)+x);
+                //textures.SetPixels(DownloadHandlerTexture.GetContent(uwr).GetPixels(),0,0);
+                textures.Apply(true);
+                material.SetTexture("_MainTexture",DownloadHandlerTexture.GetContent(uwr));
+                //material.SetTexture("_TexList",textures);
+                //material.mainTexture = DownloadHandlerTexture.GetContent(uwr);
             }
         }
     }
