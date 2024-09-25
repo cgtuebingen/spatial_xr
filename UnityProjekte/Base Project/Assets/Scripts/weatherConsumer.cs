c9d7681e375122b166f3d7e58214b0872508c6b5
Christian Hirneise <christian.hirneise@yahoo.de>
2024-09-25 13:34:28 +0200

    Collider verändert

--- UnityProjekte/Base Project/Assets/Scripts/weatherConsumer.cs
+++ UnityProjekte/Base Project/Assets/Scripts/weatherConsumer.cs
@@ -6,104 +6,69 @@ using System.Globalization;
 using System.Threading;
 using TMPro;
 
-
-public class weatherConsumer : MonoBehaviour {
-
-
+public class weatherConsumer : MonoBehaviour
+{
     [SerializeField] private float lat = 42.2f;
     [SerializeField] private float lon = 12.0f;
     [SerializeField] public string timeString = "";
-     public ClockController clockController; // Referenz auf ClockController.
+    public ClockController clockController; // Referenz auf ClockController.
 
     public DateTime time;
     public WeatherGetter weatherGetter;
     WeatherResult weather;
     bool printed = false;
 
-    // Update is called once per frame
-    void Update() {
+    int year;
+    int month;
+    int day;
+    int hour;
 
+    // In der Start-Methode die Initialisierung durchführen
+    void Start()
+    {
+        // Initialisiere die Variablen erst im Start oder Update
+        if (clockController != null)
+        {
+            year = int.Parse(clockController.yearTextSecond.text);
+            month = int.Parse(clockController.monthTextSecond.text);
+            day = int.Parse(clockController.dayTextSecond.text);
+            hour = int.Parse(clockController.hourTextSecond.text);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         // Time will be updated if the date on the clock has been changed.
-        int Day = ConvertAndLogConcatenatedInt("first_day_text", "second_day_text");
-        int Month = ConvertAndLogConcatenatedInt("first_month_text", "second_month_text");
-       
         if (clockController.isTimeChangedManually == true)
         {
-            //sleep to avoid too sensitiv input -> but kills performance... :(
-            //Thread.Sleep(500);
-            time = new DateTime(2024, Month, Day);
-            Debug.Log("time" + time);
-    
+            // Aktualisiere die Werte für Jahr, Monat, Tag, Stunde, wenn die Zeit manuell geändert wurde
+            year = int.Parse(clockController.yearTextSecond.text);
+            month = int.Parse(clockController.monthTextSecond.text);
+            day = int.Parse(clockController.dayTextSecond.text);
+            hour = int.Parse(clockController.hourTextSecond.text);
+
+            // Erstelle ein neues DateTime-Objekt mit den aktualisierten Werten
+            time = new DateTime(year, month, day, hour, 0, 0); // Stunden, Minuten, Sekunden
+
+            Debug.Log("time: " + time);
+
             weatherGetter.setRequestTime(time);
             clockController.isTimeChangedManually = false;
+
             WeatherGetter.Result<WeatherResult>? weather = weatherGetter.getWeather();
-                if (weather is WeatherGetter.Result<WeatherResult> weatherRes ) {
-                    if(!weatherRes.isOk) Debug.Log("Error");
-                    else if(!printed){
-                        Debug.Log("Weather from ID:" + weatherRes.value.weatherType);
-                        Debug.Log("Temp:" + weatherRes.value.temp);
-                        Debug.Log(weatherRes.value.city);
-                        printed = true;
+            if (weather is WeatherGetter.Result<WeatherResult> weatherRes)
+            {
+                if (!weatherRes.isOk)
+                    Debug.Log("Error");
+                else if (!printed)
+                {
+                    Debug.Log("Weather from ID:" + weatherRes.value.weatherType);
+                    Debug.Log("Temp:" + weatherRes.value.temp);
+                    Debug.Log(weatherRes.value.city);
+                    printed = true;
                 }
             }
-            //else Debug.Log("Waiting for request");
-
         }
-        
-    }
-
-    int ConvertAndLogConcatenatedInt(string gameObjectName1, string gameObjectName2)
-{
-    try
-    {
-        string concatenatedText = "";
-
-        // GameObject 1
-        GameObject gameObject1 = GameObject.Find(gameObjectName1);
-        if (gameObject1 == null)
-        {
-            throw new Exception("GameObject " + gameObjectName1 + " nicht gefunden.");
-        }
-
-        TextMeshProUGUI textComponent1 = gameObject1.GetComponent<TextMeshProUGUI>();
-        if (textComponent1 == null)
-        {
-            throw new Exception("TextMeshProUGUI-Komponente nicht gefunden für " + gameObjectName1);
-        }
-
-        concatenatedText += textComponent1.text;
-
-        // GameObject 2
-        GameObject gameObject2 = GameObject.Find(gameObjectName2);
-        if (gameObject2 == null)
-        {
-            throw new Exception("GameObject " + gameObjectName2 + " nicht gefunden.");
-        }
-
-        TextMeshProUGUI textComponent2 = gameObject2.GetComponent<TextMeshProUGUI>();
-        if (textComponent2 == null)
-        {
-            throw new Exception("TextMeshProUGUI-Komponente nicht gefunden für " + gameObjectName2);
-        }
-
-        concatenatedText += textComponent2.text;
-
-        // Conversion to Integer
-        int intValue;
-        if (!int.TryParse(concatenatedText, out intValue))
-        {
-            throw new Exception("Text konnte nicht zu Integer konvertiert werden: " + concatenatedText);
-        }
-    //     Debug.Log("int value  " + intValue);
-        return intValue;
-    }
-    catch (Exception ex)
-    {
-      Debug.LogError("Fehler in ConvertAndLogConcatenatedInt: " + ex.Message);
-        
-        throw; 
     }
 }
-
-}
-
